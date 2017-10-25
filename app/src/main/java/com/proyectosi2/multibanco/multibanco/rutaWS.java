3cc6028647f8cce6f8cc1e2e2c38e8f07ceadc7b
RONALD\Ronald Lopez <ronald_lopez_o@hotmail.com>
2017-10-24 22:53:28 -0400

    ya esta con la ruta del servidor

--- app/src/main/java/com/proyectosi2/multibanco/multibanco/rutaWS.java
+++ app/src/main/java/com/proyectosi2/multibanco/multibanco/rutaWS.java
@@ -5,7 +5,7 @@ package com.proyectosi2.multibanco.multibanco;
  */
 
 public class rutaWS {
-    public final static String LOGIN        = "http://192.168.0.30:8080/SISTEMA%20BANCARIO/public/login/";
-    public final static String DATOS        = "http://192.168.0.30:8080/SISTEMA%20BANCARIO/public/datos/";
-    public final static String HISTORICO    = "http://192.168.0.30:8080/SISTEMA%20BANCARIO/public/historia/";
+    public final static String LOGIN        = "http://ronald-apps.brazilsouth.cloudapp.azure.com/sistemaBancario/public/login/";
+    public final static String DATOS        = "http://ronald-apps.brazilsouth.cloudapp.azure.com/sistemaBancario/public/datos/";
+    public final static String HISTORICO    = "http://ronald-apps.brazilsouth.cloudapp.azure.com/sistemaBancario/public/historia/";
 }
