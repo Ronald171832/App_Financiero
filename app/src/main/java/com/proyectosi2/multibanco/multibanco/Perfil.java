4f22cc0bb4fabf7f6c612ebd2c237494e7931477
Manuel <manueljss@live.com>
2017-11-02 19:30:10 -0400

    SOY MANUEL X3

--- app/src/main/java/com/proyectosi2/multibanco/multibanco/Perfil.java
+++ app/src/main/java/com/proyectosi2/multibanco/multibanco/Perfil.java
@@ -30,7 +30,7 @@ public class Perfil extends AppCompatActivity {
     }
 
     private void iniciar() {
-        int ronald;
+        double manuel=1.0;
         infoTextView = (TextView) findViewById(R.id.infoTextView);
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
